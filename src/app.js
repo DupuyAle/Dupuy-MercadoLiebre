7030e792da8cf5f39809d13ca3eff86ccc35458e
Alejandro <dupuyalejandro04@gmail.com>
2023-08-21 19:56:13 +1200

    mercado

--- src/app.js
+++ src/app.js
@@ -1,14 +1,27 @@
-const express = require(`express`);
-const path = require(`path`);
+const express = require('express');
+const res = require('express/lib/response');
+const path = require('path');
+
 const app = express();
 
 app.use(express.static(path.join(__dirname,`../public`)))
+
+
 
 const PORT = 3001
 app.listen(PORT , () =>{
     console.log(`Servidor Funcionando en puerto ${PORT} `);
 });
 
-app.get(`/` , (req, res) => {
-    res.sendFile(path.join(__dirname, `views/index.html`));
-})
+app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/home.html"))
+});
+
+app.get("/register", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/register.html"))
+});
+
+app.get("/login", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/login.html"))
+});
+
